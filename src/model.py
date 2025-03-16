9155312bb54b1d9511789e12dffb4594b4c1a283
manhmitcf <manhmit1221@gmail.com>
2025-03-16 12:26:37 +0700

    Update model

--- src/model.py
+++ src/model.py
@@ -1,12 +1,14 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
+from torchvision.models import ResNet18_Weights  # Import weights mới
 
 class FishClassifier(nn.Module):
     def __init__(self):
         super(FishClassifier, self).__init__()
-        self.resnet = models.resnet18(pretrained=True)
-        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 6)  # Output 6 giá trị cảm quan
+        # Sử dụng weights thay vì pretrained
+        self.resnet = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, 6)  # Output 6 classes
         
     def forward(self, x):
         return self.resnet(x)
